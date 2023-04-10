cd0b828c0fd0f4e65c2d117a77e499eb450f4d97
Azat <aelchikushuulu@gmail.com>
2023-04-11 00:54:19 +0600

    Task3

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
         task1(nums);
         task2(nums);
+        task3(List.of("Hi", "Fi", "hi", "Ri", "fi", "Fi"));
     }
 
     private static void task1(List<Integer> numbers) {
@@ -26,6 +24,16 @@ public class Main {
         for (Integer number: set) {
             if (number % 2 == 0) {
                 System.out.print(number + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    private static void task3(List<String> words) {
+        System.out.println("Задание 3");
+        for (String word: words) {
+            if (Collections.frequency(words, word) == 1) {
+                System.out.print(word + " ");
             }
         }
         System.out.println();
