40a812d20614294bdc0c1ef13b6f3dd42fc1fb01
Azat <aelchikushuulu@gmail.com>
2023-04-11 00:47:10 +0600

    Task2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
         List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
         task1(nums);
+        task2(nums);
     }
 
     private static void task1(List<Integer> numbers) {
@@ -14,5 +17,17 @@ public class Main {
                 System.out.print(number + " ");
             }
         }
+        System.out.println();
+    }
+
+    private static void task2(List<Integer> numbers) {
+        System.out.println("Задание 2");
+        Set<Integer> set = new TreeSet<>(numbers);
+        for (Integer number: set) {
+            if (number % 2 == 0) {
+                System.out.print(number + " ");
+            }
+        }
+        System.out.println();
     }
 }
